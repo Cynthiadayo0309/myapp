0e0191e8f5e5e2b4c28f1d8cf9f78cb57da7a6ea
cynthiadayo <teyontt0309@gmail.com>
2021-03-27 19:40:32 +0900

    Update application

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,9 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'rails-photo-practice'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-photo-practice'
+    config.cache.storage = :fog
     config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
