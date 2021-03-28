9f9dd16462f0b48f7f0e3193fd63d2a0d97dfc1f
cynthiadayo <teyontt0309@gmail.com>
2021-03-28 16:49:39 +0900

    Update application

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,16 +4,16 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
     CarrierWave.configure do |config|
-        config.storage = :fog
-        config.fog_provider = 'fog/aws'
-        config.fog_directory = 'rails-photo-practice'
         config.fog_credentials = {
             provider: 'AWS',
             aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
             aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
             region: ENV['AWS_DEFAULT_REGION']
         }
-        config.fog_public = false
+        config.storage = :fog
+        config.fog_provider = 'fog/aws'
+        config.fog_directory = 'rails-photo-practice'
+        config.fog_public = true
     end
 end
 
