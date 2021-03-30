2003238c8d331f87e60fe9249aa1dcbbea430f3f
cynthiadayo <teyontt0309@gmail.com>
2021-03-30 12:01:55 +0900

    Update application

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
         config.fog_directory = ENV['AWS_BUCKET']
         config.fog_credentials = {
             provider: 'AWS',
-            aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-            aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+            aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+            aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
             region: ENV['AWS_DEFAULT_REGION']
         }
         config.fog_public = false
