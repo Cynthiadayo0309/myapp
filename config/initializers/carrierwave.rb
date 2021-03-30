842f5d3ab24cda037d8e2a899fdacddec6217bdb
cynthiadayo <teyontt0309@gmail.com>
2021-03-30 11:07:34 +0900

    Update application

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = ENV['AWS_BUCKET']
-    config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: ENV['AWS_DEFAULT_REGION']
-    }
+    if Rails.env.production?
+        config.storage :fog
+        config.fog_provider = 'fog/aws'
+        config.fog_directory = ENV['AWS_BUCKET']
+        config.fog_credentials = {
+            provider: 'AWS',
+            aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+            aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+            region: ENV['AWS_DEFAULT_REGION']
+        }
+        config.fog_public = false
+    end
 end
 
 # 日本語の文字化けを防ぐ
